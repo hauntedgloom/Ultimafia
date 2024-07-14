433952b4c6f723378cd440e2bd3cbb056a188f8b
ruthless <frailxup@gmail.com>
2024-07-14 18:54:59 +0000

    commenting out winner

--- Games/types/Mafia/roles/cards/GiveBlade.js
+++ Games/types/Mafia/roles/cards/GiveBlade.js
@@ -6,7 +6,7 @@ module.exports = class GiveBlade extends Card {
   constructor(role) {
     super(role);
 
-    this.actor.data.winner = "";
+    //this.actor.data.winner = "";
 
     this.meetings = {
       "Give Blade": {
