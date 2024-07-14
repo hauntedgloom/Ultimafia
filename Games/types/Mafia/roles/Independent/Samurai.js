f5abefff1fa8bd7eb2b910a0ab102d13f33abbf9
ruthless <frailxup@gmail.com>
2024-07-14 17:50:41 +0000

    test as gs

--- Games/types/Mafia/roles/Independent/Samurai.js
+++ Games/types/Mafia/roles/Independent/Samurai.js
@@ -4,6 +4,6 @@ module.exports = class Samurai extends Role {
   constructor(player, data) {
     super("Samurai", player, data);
     this.alignment = "Independent";
-    this.cards = ["VillageCore", "GiveBlade", "WinWithTown"];
+    this.cards = ["VillageCore", "GiveGun", "WinWithTown"];
   }
 };
