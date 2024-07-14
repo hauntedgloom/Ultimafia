3e7e0575c9dac495b65595478864756f7756b28d
ruthless <frailxup@gmail.com>
2024-07-14 18:06:32 +0000

    testing give blade

--- Games/types/Mafia/roles/Independent/Samurai.js
+++ Games/types/Mafia/roles/Independent/Samurai.js
@@ -4,6 +4,6 @@ module.exports = class Samurai extends Role {
   constructor(player, data) {
     super("Samurai", player, data);
     this.alignment = "Village";
-    this.cards = ["VillageCore", "WinWithVillage", "GiveGun"];
+    this.cards = ["VillageCore", "WinWithVillage", "GiveBlade"];
   }
 };
