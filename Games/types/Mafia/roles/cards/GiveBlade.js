c6871226e01e66f4935b7f1cad923c552297337f
ruthless <frailxup@gmail.com>
2024-07-14 18:16:39 +0000

    Testing the stats to see if it works

--- Games/types/Mafia/roles/cards/GiveBlade.js
+++ Games/types/Mafia/roles/cards/GiveBlade.js
@@ -5,6 +5,19 @@ module.exports = class GiveBlade extends Card {
   constructor(role) {
     super(role);
 
+    this.actor.data.winner = "";
+    this.actor.data.hp = 150;
+    this.target.data.hp = 150;
+
+    this.actor.data.atk = 15;
+    this.target.data.atk = 15;
+
+    this.actor.data.def = 10;
+    this.target.data.def = 10;
+
+    this.actor.data.crit = 1.0;
+    this.target.data.crit = 1.0;
+
     this.meetings = {
       "Give Blade": {
         states: ["Night"],
